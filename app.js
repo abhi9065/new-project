9721c8c51754d19a862324e2d7455cb05cf0307e
abhi9065 <110813322+abhi9065@users.noreply.github.com>
2022-12-30 11:46:01 +0530

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+import yahoo from "./main.js" 
+
+yahoo()
+
+
+/*import * as yahoo from "./main.js"
+
+console.log(yahoo.message)
+
+console.log(yahoo.user("yahoo baba"))
+
+
+let a = new yahoo.test()*
---
+++ main.js
@@ -0,0 +1,18 @@
+ 
+ export default function(){
+    console.log("hello")
+ }
+ 
+ /*let message = "es6 modules"
+
+ function user(name){
+    return `hello ${name}`
+}
+
+ class test{
+    constructor(){
+        console.log("hey hero")
+    }
+}
+
+export {message,user,test}*/
